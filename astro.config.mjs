87717a1f5c3b49188fe2e1c151498d8fbb471f47
vishakh-abhayan <vishakhabhayan0@gmail.com>
2026-05-18 22:25:14 +0530

    chore(landing_page): add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.wynautlabs.com'
+  site: 'https://www.wynautlabs.com',
+  integrations: [sitemap()]
 });
